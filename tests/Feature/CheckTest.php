7abdd013d1abbdf919dd3f223756cb3220f28c72
Andrey Frolov <internet.buro@gmail.com>
2022-08-26 17:19:51 +0300

    Change testCreateCheck with use Http::fake

--- tests/Feature/CheckTest.php
+++ tests/Feature/CheckTest.php
@@ -4,6 +4,8 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
 use Tests\TestCase;
 
 class CheckTest extends TestCase
@@ -12,11 +14,24 @@ class CheckTest extends TestCase
 
     public function testCreateCheck()
     {
-        $id = 9;
+        $urlName = 'http://test.ru';
+
+        $id = DB::table('urls')->insertGetId([
+            'name' => $urlName,
+            'created_at' => now()
+        ]);
+
+        Http::fake([
+            $urlName => Http::response('ok', 200, [])
+        ]);
+
         $response = $this->post(route('check_post', $id));
 
         $response->assertRedirect();
 
-        $this->assertDatabaseHas('url_checks', ['url_id' => $id]);
+        $this->assertDatabaseHas('url_checks', [
+            'url_id' => $id,
+            'status_code' => 200
+        ]);
     }
 }
