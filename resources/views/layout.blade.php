8e256529140de7bde094112b8fd418ccfc83e38f
Andrey Frolov <internet.buro@gmail.com>
2022-09-18 21:16:29 +0300

    Change bootstrap including from CDN to Vite building

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,7 @@
 
     <title>Анализатор страниц</title>
 
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="min-vh-100 d-flex flex-column">
 <header class="flex-shrink-0">
@@ -33,7 +32,5 @@
         @yield('content')
 </main>
 
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
